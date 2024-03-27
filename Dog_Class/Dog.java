808a08d1d37839ee8f7e87cbf8768494b7de704d
Bulela <bulelagomoshe1@gmail.com>
2024-03-27 11:46:51 +0200

    Created a dog class, instance/object, method called bark

---
+++ Dog_Class/Dog.java
@@ -0,0 +1,16 @@
+package Dog_Class;
+
+public class Dog {
+
+    public static void main(String[] args) {
+        // Create an instance of the Dog class
+        Dog myDog = new Dog();
+        
+        // Call the bark() method on the instance
+        myDog.bark();
+    }
+
+    public void bark() {
+        System.out.println("Hoof. . .. Hoof . . . Hoof");
+    }
+}
